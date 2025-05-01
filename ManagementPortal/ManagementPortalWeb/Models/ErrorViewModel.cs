dd06cb5fe12d7f78aef8a091801840226abaf960
Yuvraj Singh <154751919+Yuvraj-pb23@users.noreply.github.com>
2025-05-01 21:44:27 +0530

    Delete ManagementPortal directory

--- ManagementPortal/ManagementPortalWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ManagementPortalWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
