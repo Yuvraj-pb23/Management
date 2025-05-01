cb5857e4107ca43539154520b2850cab7c753020
Yuvraj Singh <yuvrajjabbal@gmail.com>
2025-05-01 21:40:50 +0530

    manager

---
+++ ManagementPortal/ManagementPortalWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ManagementPortalWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
